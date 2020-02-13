10624ac34ef4a2c0607b8e8488e1c8d9dcfc344f
Ben Inada <me@beninada.com>
2020-02-13 08:25:23 -0800

    fix create playlist bug

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -26,7 +26,7 @@ class Playlist extends Model
     public static $createRules = [
         'title' => ['required', 'string', 'max:100'],
         'description' => ['required', 'string', 'max:5000'],
-        'user_id' => ['required', 'in:users'],
+        'user_id' => ['required', 'exists:users,id'],
     ];
 
 
